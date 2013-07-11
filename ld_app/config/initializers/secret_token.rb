af0af1cf759137011b0a674114eb1d2a3105ccb9
johnh4 <jehowl4@gmail.com>
2013-07-11 15:14:59 -0400

    the rest

---
+++ ld_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LdApp::Application.config.secret_key_base = '9ef72cdda41efb12f117bc71aaae366649b24cf2c47ce45c7456c1ff8d0982bbba577f65eb9cf32a195eb538a986a4239beacb486e3d3afb5b3b65cfa18e2393'
